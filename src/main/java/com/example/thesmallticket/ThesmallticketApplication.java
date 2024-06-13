e27435c875d723fd60efa473f4880ff1b29a7862
simonebarberini <simonebarberini02@gmail.com>
2024-06-13 16:59:00 +0200

    first commit

---
+++ src/main/java/com/example/thesmallticket/ThesmallticketApplication.java
@@ -0,0 +1,13 @@
+package com.example.thesmallticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesmallticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesmallticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thesmallticket/ThesmallticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thesmallticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesmallticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
